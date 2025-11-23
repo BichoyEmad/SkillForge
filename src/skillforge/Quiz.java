ef45f80335380654d8b35721c54d81056781ef18
hp <hp@LAPTOP-MTT3I4AQ>
2025-11-23 19:16:56 +0300

    Adding Quiz

---
+++ src/skillforge/Quiz.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package skillforge;
+
+/**
+ *
+ * @author hp
+ */
+import java.util.*;
+
+public class Quiz {
+    private String quizId;
+    private List<Question> questions = new ArrayList<>();
+
+    public Quiz() { this.quizId = java.util.UUID.randomUUID().toString(); }
+
+    public List<Question> getQuestions() { return questions; }
+    public void addQuestion(Question q) { questions.add(q); }
+    public String getQuizId() { return quizId; }
+}
