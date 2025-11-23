9207e58c2f3bc6c0e335d3d87633c43f12fd8c88
hp <hp@LAPTOP-MTT3I4AQ>
2025-11-23 19:15:57 +0300

    Adding Question

---
+++ src/skillforge/Question.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package skillforge;
+
+/**
+ *
+ * @author hp
+ */
+import java.util.*;
+
+public class Question {
+    private String questionText;
+    private List<String> options = new ArrayList<>();
+    private int correctOptionIndex;
+
+    public Question() {}
+    public Question(String questionText, List<String> options, int correctOptionIndex) {
+        this.questionText = questionText;
+        this.options = options;
+        this.correctOptionIndex = correctOptionIndex;
+    }
+
+    public String getQuestionText() { return questionText; }
+    public List<String> getOptions() { return options; }
+    public int getCorrectOptionIndex() { return correctOptionIndex; }
+}
